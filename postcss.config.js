bcde57bcec19cf3997c389eeed4f03cf95886210
Steve Cochrane <steve@stevecochrane.com>
2020-02-26 22:12:19 -0600

    Add cssnano and PurgeCSS to PostCSS flow

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,23 @@
+const cssnano = require('cssnano');
 const postcssPresetEnv = require('postcss-preset-env');
+const purgecss = require('@fullhuman/postcss-purgecss');
 const tailwindcss = require('tailwindcss');
 
+const plugins = [
+  tailwindcss(),
+  postcssPresetEnv(),
+  cssnano({ preset: 'default' })
+];
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(
+    purgecss({
+      content: ['./dist/**/*.html'],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    })
+  );
+}
+
 module.exports = {
-	plugins: [
-		tailwindcss(),
-    postcssPresetEnv()
-	]
+	plugins
 };
